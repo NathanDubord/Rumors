5a7fadc9338e79c486af7e5ba33c0499ec865627
Nathan Dubord <nathandubord@gmail.com>
2020-03-10 18:14:09 -0400

    Update file structure

---
+++ rumours/src/components/app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../assets/images/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1>Hello</h1>
+  );
+}
+
+export default App;
