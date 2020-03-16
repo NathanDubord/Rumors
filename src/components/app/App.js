1ff3705dd9df56e0d835393ab9c116cd8c8962bd
Nathan Dubord <nathandubord@gmail.com>
2020-03-15 21:05:59 -0400

    Start of round trip

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+
+import axios from 'axios';
 import Button from '@material-ui/core/Button';
 
 import './App.scss';
 
 function App() {
+  axios
+    .get('/api/home/')
+    .then(response => {
+      alert(JSON.stringify(response.data))
+    })
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </>
   )
 }
 
